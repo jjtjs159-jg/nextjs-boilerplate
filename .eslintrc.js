ada94def894406a408c7fa970aee57e6f8aa8eb6
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2023-06-03 05:29:27 +0900

    feat: eslint 사용하지 않는 변수에 대한 에러를 경고로 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['next', 'prettier'],
   rules: {
     'no-unused-vars': [
-      'error',
+      'warn',
       {
         args: 'after-used',
         caughtErrors: 'none',
