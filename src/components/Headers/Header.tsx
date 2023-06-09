df6141bfae69503356358601aa181394bffa3ab2
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2023-06-10 02:06:16 +0900

    feat: Header 컴포넌트 Navbar 분리 정의
    next/navigation

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -1,9 +1,26 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import Navbar from './Navbar';
+
 import { AiOutlineBell, AiOutlineGithub } from 'react-icons/ai';
 
 export default function Header() {
+  const navLinks = [
+    {
+      name: 'docs',
+      href: '/docs',
+    },
+    {
+      name: 'components',
+      href: '/ui',
+    },
+    {
+      name: 'about',
+      href: '/about',
+    },
+  ];
+
   return (
     <header className="sticky top-0 z-40 w-full bg-gray-300 border-b dark:bg-red-500">
       <div className="mx-auto max-w-7xl">
@@ -13,25 +30,7 @@ export default function Header() {
               <Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} priority />
             </Link>
             <div className="flex items-center ml-auto">
-              <nav className="text-sm font-semibold leading-6 text-slate-800">
-                <ul className="flex space-x-8">
-                  <li>
-                    <Link href="/" className="hover:text-green-700">
-                      Docs
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/" className="hover:text-green-700">
-                      Components
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/" className="hover:text-green-700">
-                      About
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
+              <Navbar navLinks={navLinks} />
               <div className="flex items-center pl-6 ml-6 border-l border-gray-400">
                 <button type="button" aria-label="Alert">
                   <AiOutlineBell className="w-6 h-6" />
---
+++ src/components/Headers/Navbar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+type Props = {
+  navLinks: {
+    name: string;
+    href: string;
+  }[];
+};
+
+export default function Navigation({ navLinks }: Props) {
+  const pathname = usePathname();
+
+  return (
+    <nav className="text-sm font-bold leading-6 text-slate-800">
+      <ul className="flex space-x-8">
+        {navLinks.map((link) => {
+          const isActive = pathname.startsWith(link.href);
+
+          return (
+            <li key={link.name}>
+              <Link
+                href={link.href}
+                className={
+                  isActive
+                    ? 'text-blue-700 hover:text-green-700'
+                    : 'text-gray-600 hover:text-green-700'
+                }
+              >
+                {link.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
